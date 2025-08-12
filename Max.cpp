bd3c1b23cc5cabed75d5e20215b2548a67b3c43b
Gaurav <Shivalegaurav487@gmail.com>
2025-08-12 20:52:06 +0530

    Update and rename maximum_of_three_Version2.cpp to Max.cpp

---
+++ Max.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+#include<conio.h>
+int main() {
+    int a, b, c;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+    if (b > c && b>=a){
+    cout<<"Maximum number is: "<<b;
+    }
+    cout<<"Maximum number is: "<<c;
+    }
+    getch();
+    return 0;
+}
