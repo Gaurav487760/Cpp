67959f1cbdb0fe399c89020d01cbbf9b0b7225ea
Gaurav <Shivalegaurav487@gmail.com>
2025-08-14 12:34:54 +0530

    Update and rename area_of_triangle.cpp to AreaTriangle.cpp

--- AreaTriangle.cpp
+++ AreaTriangle.cpp
@@ -1,18 +1,11 @@
-#include <iostream>
-using namespace std;
-
+#include <iostream.h>
 int main() {
     double base, height, area;
-
-    cout << "Enter base of the triangle: ";
+    cout << "Enter base of the Triangle: ";
     cin >> base;
-
-    cout << "Enter height of the triangle: ";
+    cout << "Enter height of the Triangle: ";
     cin >> height;
-
     area = 0.5 * base * height;
-
     cout << "Area of the triangle is: " << area << endl;
-
     return 0;
 }
