a02ac29e6f45aa00afbc62023b9a04fd78ee909b
Gaurav <Shivalegaurav487@gmail.com>
2025-08-14 12:35:58 +0530

    Update and rename area_of_square.cpp to Area_square.cpp

---
+++ Area_square.cpp
@@ -0,0 +1,9 @@
+#include <iostream.h>
+int main() {
+    float side, area;
+    cout << "Enter the length of the side of the Square: ";
+    cin >> side;
+    area = side * side;
+    cout << "Area of the Square = " << area << endl;
+    return 0;
+}
